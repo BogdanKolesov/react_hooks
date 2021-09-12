78bafcc5fed3d38272d6149bfea9daea5ade1815
Bogdan <bogdan_kolesov@bk.ru>
2021-09-12 12:30:14 +0600

    add: UseEffectHook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import UseStateHook from './hooks/UseStateHook'
+import UseEffectHook from './hooks/UseEffectHook'
+
 
 function App() {
   return (
     <div className="App">
-      <UseStateHook />
+      {/* <UseStateHook /> */}
+      <UseEffectHook />
     </div>
   );
 }
